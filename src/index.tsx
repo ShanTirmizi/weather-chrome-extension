791c364041fa1cd3ec32dc638a137f7c3611d4a0
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2023-10-26 06:58:33 +0100

    added an options page

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import Popup from './App';
+
+
+function init() {
+  const appContainer = document.querySelector('#app-container');
+  if (!appContainer) {
+    throw new Error('Can not find #app-container');
+  }
+  const root = createRoot(appContainer);
+  root.render(<Popup />);
+}
+
+init();
