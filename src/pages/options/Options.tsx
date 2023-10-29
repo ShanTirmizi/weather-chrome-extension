a4c4841a52980044cbab0dfab2b753666d12796e
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2023-10-29 11:52:02 +0000

    @@

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -53,27 +53,50 @@ useEffect(() => {
 }, [options]);
 
 
-  if (!options) return <p>Loading...</p>
+  if (!options) return <p className="text-center mt-10 text-xl text-black">Loading...</p>;
   return (
-    <>
-      <div className="container text-lime-400">
-        <form onSubmit={(e) => getCityWeather(e)}>
-          <input type="text" value={defaultCity} onChange={(e) => setDefaultCity(e.target.value)} />
-          <button type="submit" disabled={!options}>Save</button>
+    <div className="min-h-screen bg-warmGray-100 flex flex-col items-center justify-center">
+      <div className="container max-w-lg p-6 rounded-xl shadow-lg bg-yellow-300">
+        <h1 className="text-2xl mb-5 text-center text-black"><b>Weather Options</b></h1>
+        <form onSubmit={(e) => getCityWeather(e)} className="mb-4">
+          <label htmlFor="city" className="block text-sm font-medium text-black mb-2">Select a default city</label>
+          <input 
+            className="w-full px-3 py-2 border rounded-md text-black bg-yellow-200" 
+            id="city"
+            type="text" 
+            value={defaultCity}
+            placeholder="City name"
+            onChange={(e) => setDefaultCity(e.target.value)} 
+          />
+          <button 
+            className="mt-3 w-full bg-black text-yellow-200 p-2 rounded-md hover:bg-gray-800 transition duration-300 cursor-pointer" 
+            type="submit" 
+            disabled={!options}>
+            Save
+          </button>
         </form>
+        <div className="mb-4">
+          <label htmlFor="scale" className="block text-sm font-medium text-black mb-2">Temperature Scale</label>
+          <select 
+            className="w-full px-3 py-2 border rounded-md text-black cursor-pointer bg-yellow-200"
+            id="scale"
+            name="option" 
+            value={options.tempScale} 
+            onChange={(e) => setOptions({ ...options, tempScale: e.target.value } as LocalStorageOptions)}
+          >
+            <option value="metric">Celsius</option>
+            <option value="imperial">Fahrenheit</option>
+          </select>
+        </div>
+        {error && <p className="text-red-500 mb-2">{error}</p>}
+        {cityData && (
+          <div className="text-center text-black border border-black mx-2 rounded-md">
+            <p className="text-xl font-bold">{cityData?.name}</p>
+            <p className="text-lg">{cityData?.main.temp}°</p>
+          </div>
+        )}
       </div>
-      <div className="container text-lime-400">
-        <select name="option" value={options.tempScale} onChange={(e) => setOptions({ ...options, tempScale: e.target.value } as LocalStorageOptions)}>
-          <option value="metric">Celsius</option>
-          <option value="imperial">Fahrenheit</option>
-        </select>
-      </div>
-      <div className="container text-lime-400">
-        <p>{error}</p>
-        <p>{cityData?.name}</p>
-        <p>{cityData?.main.temp}</p>
-      </div>
-    </>
+    </div>
   );
 };
 
